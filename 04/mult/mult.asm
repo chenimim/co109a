3ede606cfbfbdb498fbc750688507c80668a4943
Chenim <chen890430@gmail.com>
2021-01-16 14:10:59 +0800

    HW

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -8,62 +8,54 @@
 
 // Put your code here.
 
-//<pre><code>
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+//#include<stdio.h>
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+//int main()
+//{
+    //int R0=5;
 
-// Put your code here.
-//16384到24575 為螢幕
-//24576為鍵盤
-@16383
-D=A //將值先輸入D
-@20
-M=D //R20存入控制螢幕初始值
-@21
-M=D //R21存入控制螢幕初始值
-@8192
-D=A //存入計數器控制螢幕反白
-M=D
-@8193
-M=D //存入計數器控制螢幕變黑
-@24576
-D=M 
-@27
-D;JEQ //當沒按鍵盤跳到迴圈(LOOP1反白)
-@20   //當按鍵盤跳到迴圈(LOOP2反黑)[迴圈LOOP2開頭]
-M=M+1 //A+1(16383+1......)
-D=M   
-A=D
-M=-1 //反黑指令
-@8193 
-M=M-1 //計數器-1
-D=M
-@39  
-D;JEQ //當計數器0跳出迴圈LOOP2
-@15
-0;JMP //回到LOOP2迴圈
-@21 //當沒按鍵盤跳到迴圈(LOOP1反白)[迴圈LOOP2開頭]
-M=M+1 //A+1(16383+1......)
-D=M
-A=D
-M=0 //反白指令
-@8192
-M=M-1 //計數器-1
-D=M
-@39
-D;JEQ //當計數器0跳出迴圈LOOP1
-@27
-0;JMP //回到LOOP1迴圈
-@0
-0;JMP //回到主迴圈指令1
-//<code></pre>
+     //@5
+     //D=A
+     //@0
+     //M=D
+
+    //int R1=5;
+     //@3
+     //D=A
+     //@1
+     //M=D
+
+    //int R2=0;
+
+     @2
+     M=0
+
+    //LOOP:
+        (loop)
+        //if(R1==0) goto EXIT;
+
+         @1
+         D=M
+         @exit
+         D;JEQ 
+
+        //R2+=R0;
+
+         @0
+         D=M
+         @2
+         M=M+D
+         //R1--;
+         @1
+         M=M-1
+        
+
+        //goto LOOP;
+        @loop
+        0;JMP
+    //EXIT:
+    (exit)
+    @exit
+    0;JMP
+    //return 0;
+//}
--- hw/hw8.md
+++ hw/hw8.md
@@ -1,39 +1,26 @@
 # HW8
 
-* CODE
-
-  ```
-    // This file is part of www.nand2tetris.org
-    // and the book "The Elements of Computing Systems"
-    // by Nisan and Schocken, MIT Press.
-    // File name: projects/04/Mult.asm
-
-    // Multiplies R0 and R1 and stores the result in R2.
-    // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-
-    // Put your code here.
-    @2 
-    M=0
-    @0
-    D=M
-    @50 
-    D;JEQ 
-    @1
-    D=M
-    @50 
-    D;JEQ
-    @12 
-    D;JNE
-    @0
-    D=M
-    @2
-    M=M+D 
-    @1
-    M=M-1
-    D=M
-    @12 
-    D;JNE
-    ```
+* code
+```
+@0
+D=A //先將D設0
+@2  
+M=D //將結果先規0
+@0  //LOOP1
+D=M //將R0設為計數器
+@16
+D;JEQ //D等於0跳出迴圈
+@1
+D=M  //將值輸入D
+@2
+M=D+M //將D值加入R2
+@0
+M=M-1 //計數器-1
+@4  
+0;JMP //回到LOOP1
+@16
+0;JMP
+```
 * PICTURE
 
     <img src="計算機/hw8.jpg" >
